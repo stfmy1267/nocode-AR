451c62d8abad158acd8ffd582f02b4ead457c632
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2022-07-15 14:28:43 +0900

    Updates

--- src/components/MainVisual.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="h-[580px] w-full flex justify-around content-around items-center">
-        <h2 class="text-4xl leading-[3rem]">誰でも簡単に作れる<br>WebARスタンプラリー</h2>
-        <img src="../assets/Mockup-1.png" alt="メイン画像" class="w-64">
-    </div>
-</template>

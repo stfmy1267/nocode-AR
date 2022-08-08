7ad051e44f18b179f3b9babe651e2789deee7838
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2022-08-09 01:06:58 +0900

    setting githun pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   // base: './',
   base: (process.env.NODE_ENV === 'poduction')
-    ? '/[nocode-ar]/' : './',
+    ? '/nocode-ar/' : './',
   build: {
   outDir: 'docs'
 },

4b800a0b76046d90ab3127fe19098284ad9dbabb
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2022-08-09 00:53:20 +0900

    setting githun pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import viteImagemin from 'vite-plugin-imagemin'; // 追加
-
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  base: './',
+  // base: './',
+  base: (process.env.NODE_ENV === 'poduction')
+    ? '/[nocode-ar]/' : './',
+  build: {
+  outDir: 'docs'
+},
   server: {
     port: 8888
   },
   plugins: [
     vue(),
-  ]
+  ],
   //   // 画像圧縮
   //   viteImagemin({
   //     gifsicle: {
